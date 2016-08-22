1abe247400a3165064ec957f6f57ffc2033f3cde
Ege Ersoz <ege.ersoz@gmail.com>
2016-08-21 20:49:55 -0500

    completed table formatter and tests

--- mix.exs
+++ mix.exs
@@ -3,8 +3,10 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     escript: escript_config,
      version: "0.1.0",
      elixir: "~> 1.3",
+     name: "Github Issue Displayer",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -14,7 +16,7 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +29,15 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      httpoison: "~> 0.8",
+      poison:    "~> 1.5",
+      ex_doc:    "~> 0.11",
+      earmark:   ">= 0.0.0"
+    ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
